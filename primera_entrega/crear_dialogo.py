79cc8e6b049db487e166f26d2882bd9c244edb3a
Mariano <mariano.m3n@gmail.com>
2017-04-26 15:13:53 -0300

    crea actor.txt

--- primera_entrega/crear_dialogo.py
+++ primera_entrega/crear_dialogo.py
@@ -4,33 +4,43 @@
 import pilasengine, random, os
 
 
-def guardo_dialogo_actor(actor,dialogo):
+def guardo_archivo_actor(actor,dialogo):
 # Genera NombreActor.txt --> dialogo(lines)
-    arch = actor+'.txt'
+    d = dialogo.strip()
+    act = actor.replace(' ','')
+    arch = act+'.txt'
     with open(arch, 'a') as f:
-        f.write(dialogo)
+        f.write(d+"\n")
     f.close()
 
-guion = open('guion.txt')
+guion = open('guion.txt','r')
+lines = guion.readlines()
 #print list(guion)
 secuencia_dialogo = []
-cant_actores = set(secuencia_dialogo)
 # Genero secuencia y linieas de cada actor
-for dialogo in guion:
-    d = dialogo.split(':')
-    secuencia_dialogo.append(d[0])
-    guardo_dialogo_actor(d[0],d[1])
+actor_dialogo = {}
+for dialogo in lines:
+    l = dialogo.split(':')
+    print(l[1])
+    secuencia_dialogo.append(l[0])
+    guardo_archivo_actor(l[0],l[1])
+    
+    try:
+	actor_dialogo[l[0]].append(l[1])
+    except(KeyError): 
+	actor_dialogo[l[0]] = []
+	actor_dialogo[l[0]].append(l[1])
 
 
+for v in actor_dialogo.values():
+    print(v)
 
-
-
-# lee linea del actor
-for actor in secuencia_dialogo:
-    arch = actor+'.txt'
-    f = open(arch, 'rw+')
-    print f.readline() 
-# cantidad de actoes en la escena referencia para crear actores en pilas
+## lee linea del actor
+#for actor in secuencia_dialogo:
+#    arch = actor+'.txt'
+#    f = open(arch, 'rw+')
+#    print f.readline() 
+## cantidad de actoes en la escena referencia para crear actores en pilas
 actores = set(secuencia_dialogo)
 
 print len(actores)

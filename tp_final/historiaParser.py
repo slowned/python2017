115e6c190eb217e48cb8db8b98b5d25c4865b04f
Mariano <mariano.m3n@gmail.com>
2017-05-30 16:14:21 -0300

    parsea historia en escenas

---
+++ tp_final/historiaParser.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import io, os
+
+class HistoryParser(object):
+
+    def __init__(self):
+        self.scenes_parser()
+
+    def save_scene(self,scene,line):
+        # Genera Escena.txt --> dialogo(lines)
+        print (scene)
+        filename = "{0}.txt".format(scene)
+        with open(filename, 'a') as f:
+            f.write(line.encode("utf-8") + os.linesep)
+
+    def new_scene(self,line):
+        l = line.strip('[').strip(']').replace(' ','').split(',')
+        return l[0]
+
+    def scenes_parser(self):
+        history = io.open('historia.txt', "r", encoding="utf-8")
+        lines = history.readlines()
+        history.close()
+
+        for line in lines:
+            if (line[0] == '['):
+                scene = self.new_scene(line)
+            self.save_scene(scene,line)
+
+
+h = HistoryParser()

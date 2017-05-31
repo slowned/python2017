2f7f11815218b0549049702d7a6277cd184bc640
Tati <admin@Mac-Pro-de-Tati.local>
2017-05-31 01:34:15 -0300

    agrega pseudo-codigo

--- tp_final/CuentoPersonalizado.py
+++ tp_final/CuentoPersonalizado.py
@@ -6,7 +6,9 @@ from historiaParser import HistoryParser
 """-------------se crea la escena--------------------"""
 
 class Scene(object):
-    def 
+    def __init__(self):
+	self.name = ''
+	self.script_parcer()
 
     def guion_escenea(self):
         guion = io.open('escena1.txt', "r", encoding="utf-8")
@@ -18,6 +20,19 @@ class Scene(object):
         secuencia_dialogo = []
 
         for linea in lineas:
-            if (line[0] == '['):
-                scene = HistoryParser.new_scene(line)
+            if line[0] == '[':
+                self.name = HistoryParser.new_scene(line)
+	    elif line[0] == '(':
+		print 'agreo accion a la lista_secuencia'
+	    elif line[0] == '{':
+		print 'agrego decicion a lista_secuncia'
+	    else:
+		print 'agrego actor a lista_secuencia'
+		    try:
+			actor_dialogo[l[0]].append(dm)
+		    except(KeyError): 
+			actor_dialogo[l[0]] = []
+			actor_dialogo[l[0]].append(dm)
+
+	    	
     

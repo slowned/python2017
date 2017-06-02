0d871bd4499f2f5ca4646544009751326478e907
Mariano <mariano.m3n@gmail.com>
2017-06-02 02:01:11 -0300

    pseudo-codigo

--- tp_final/CuentosPersonalizado.py
+++ tp_final/CuentosPersonalizado.py
@@ -5,17 +5,64 @@
 from historiaParser import HistoryParser
 from escena import Scene
 from crear_actor import ActorPelicula
+import pilasengine
+
+pilas = pilasengine.iniciar()
+
+def actuando(actor,secuencia):
+    try:
+        a=actor[secuencia[0]]
+        actor[secuencia[0]].decir(actor[secuencia[0]].get_linea())
+        secuencia.remove(secuencia[0])
+    except IndexError:
+        if es_accion(secuencia[0]):
+            pilas.avisar("ACCION")
+        elif es_decision(secuendia[0]):
+            pilas.avisar("DECISION")
+        else:
+            pilas.avisar("FIN")
 
 h = HistoryParser()
-s = Scene()
+s = Scene(h.escena[0])
+escenas = []
 
-papeles = set(h.secuencia())
+for i in len(h.escenas):
+   escenas.append(Scene(i)) 
 
-for actor in papeles:
-    xy = pos[0]
-    lineas = actor_dialogo[actor] 
-    act = ActorPelicula(pilas,nombre=actor,position=pos,dialogo=lineas)
-    actores[act.get_nombre()] = act
-    pos.remove(xy)
+"""------main menu---------"""
+bg = pilas.imagenes.cargar('imagenes/'+random.choice(os.listdir('imagenes/')))# if imagen else "imagenes/ribbit7-300px.png"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#b = pilas.interfaz.Boton("empezar dialogo")
+#b.y = -125
+#b.x = 125
+#
+#
+#b.conectar(lambda : actuar(s.actores,s.secuencia))
+
+
+pilas.ejecutar()
